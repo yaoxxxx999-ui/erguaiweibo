c2f1338577c3737aead92b4993bc6992a4f8b51c
yaoxxxx999-ui <yaoxxxx999@gmail.com>
2026-06-11 17:44:39 +0800

    Refresh weibo archive cache strategy

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const cacheName = "erguai-weibo-v7";
+const cacheName = "erguai-weibo-v8";
 const assets = [
   "./",
   "./index.html",
@@ -18,6 +18,7 @@ const assets = [
 ];
 
 self.addEventListener("install", (event) => {
+  self.skipWaiting();
   event.waitUntil(caches.open(cacheName).then((cache) => cache.addAll(assets)));
 });
 
@@ -25,10 +26,25 @@ self.addEventListener("activate", (event) => {
   event.waitUntil(
     caches.keys().then((keys) => {
       return Promise.all(keys.filter((key) => key !== cacheName).map((key) => caches.delete(key)));
-    })
+    }).then(() => self.clients.claim())
   );
 });
 
 self.addEventListener("fetch", (event) => {
+  const url = new URL(event.request.url);
+
+  if (url.pathname.endsWith("/weibos.txt")) {
+    event.respondWith(
+      fetch(event.request)
+        .then((response) => {
+          const copy = response.clone();
+          caches.open(cacheName).then((cache) => cache.put(event.request, copy));
+          return response;
+        })
+        .catch(() => caches.match(event.request))
+    );
+    return;
+  }
+
   event.respondWith(caches.match(event.request).then((cached) => cached || fetch(event.request)));
 });
